755ed918c3691946a6eb313dbde30b8cf87b21dd
fabrego <nando.1992.40@gmail.com>
2015-06-21 16:40:24 -0500

    Cambio de impresion de mensaje

--- Sistema de Inventario/src/Sistema_de_Inventario/Startup.cs
+++ Sistema de Inventario/src/Sistema_de_Inventario/Startup.cs
@@ -23,6 +23,7 @@ namespace Sistema_de_Inventario
                 Console.WriteLine("Otro Cambio");
                 Console.WriteLine("De nuevo");
                 Console.WriteLine("No jodas jajaj");
+
                 await context.Response.WriteAsync("Hello World!");
             });
         }
