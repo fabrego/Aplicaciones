2b60f138fd76890bd1c41dd63e1ce7931a016208
fabrego <nando.1992.40@gmail.com>
2015-06-21 17:24:22 -0500

    Verificando este cambio afuera en el namespace

--- Sistema de Inventario/src/Sistema_de_Inventario/Startup.cs
+++ Sistema de Inventario/src/Sistema_de_Inventario/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Framework.DependencyInjection;
 
 namespace Sistema_de_Inventario
 {
+    //Verificando este cambio afuera en el namespace
     public class Startup
     {
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
